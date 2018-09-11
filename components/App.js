1832d66b913bb6c18bfa9a535070a54c88f92b82
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-11 16:13:56 +0200

    Modify App.js

--- components/App.js
+++ components/App.js
@@ -1,5 +1,5 @@
 App = React.createClass({
-	
+
 	getInitialState() {
 		return {
 			loading: false,
@@ -7,37 +7,41 @@ App = React.createClass({
 			gif: {}
 		};
 	},
-	
-	handleSearch: function(searchingText) {  
+
+	handleSearch: function(searchingText) {
 		this.setState({
-			loading: true  
+			loading: true
 		});
-		this.getGif(searchingText, function(gif) { 
-			this.setState({  
-				loading: false, 
-				gif: gif,  
-				searchingText: searchingText  
+		this.getGif(searchingText).then((gif) => {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
 			});
-		}.bind(this));
+		})
 	},
-	
-	getGif: function(searchingText, callback) { 
-		var url = 'https://api.giphy.com/v1/gifs/random?api_key=0867McZQVsPSo5gU6CrwFSyoc6mS31lM&tag=' + searchingText; 
-		var xhr = new XMLHttpRequest();  
-		xhr.open('GET', url);
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-			   var data = JSON.parse(xhr.responseText).data; 
-				var gif = {  
-					url: data.fixed_width_downsampled_url,
-					sourceUrl: data.url
-				};
-				callback(gif); 
-			}
-		};
-		xhr.send();
+
+	getGif: function(searchingText) {
+		return new Promise((resolve, reject) => {
+			var url = 'https://api.giphy.com/v1/gifs/random?api_key=0867McZQVsPSo5gU6CrwFSyoc6mS31lM&tag=' + searchingText;
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+				  var data = JSON.parse(xhr.responseText).data;
+					var gif = {
+						url: data.fixed_width_downsampled_url,
+						sourceUrl: data.url
+					};
+					resolve(gif);
+				} else {
+						reject(new Error(xhr.statusText));
+					}
+			};
+			xhr.send();
+		});
 	},
-	
+
     render: function() {
 
         var styles = {
@@ -51,11 +55,11 @@ App = React.createClass({
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
-            <Gif 
-				loading={this.state.loading}
-				url={this.state.gif.url}
-				sourceUrl={this.state.gif.sourceUrl}
-			/>
+          <Gif
+						loading={this.state.loading}
+						url={this.state.gif.url}
+						sourceUrl={this.state.gif.sourceUrl}
+					/>
           </div>
         );
     }
